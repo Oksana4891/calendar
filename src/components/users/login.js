4f72f5ba4ad088aa2736e979750ebca7d790dcbd
Oksana Omelchenko <kuzheloksana@gmail.com>
2021-02-24 12:15:24 +0200

    fix user name type in section login

--- src/components/users/login.js
+++ src/components/users/login.js
@@ -1,44 +1,42 @@
-import refs from "../../utils/refs";
-import  User from './user';
+import refs from '../../utils/refs';
+import User from './user';
 import Admin from './admin';
 import userArr from './addUsers';
-import openPopup from "../formEvent/open";
-import removeEvent from "../table/removeEvent";
+import openPopup from '../formEvent/open';
+import removeEvent from '../table/removeEvent';
 
+function login(e) {
+  e.preventDefault();
+  refs.bntOpenPopupAddEvent.removeEventListener('click', openPopup);
+  refs.table.removeEventListener('click', removeEvent);
+  refs.bntOpenPopupAddEvent.disabled = true;
+  refs.bntOpenPopupAddEvent.classList.remove('is-active');
+  refs.table.classList.remove('is-active');
 
-function login (e) {
-    e.preventDefault();
-    refs.bntOpenPopupAddEvent.removeEventListener('click', openPopup);
-    refs.table.removeEventListener('click', removeEvent);
-    refs.bntOpenPopupAddEvent.disabled=true;
-    refs.bntOpenPopupAddEvent.classList.remove('is-active'); 
-    refs.table.classList.remove('is-active');
+  const value = e.target.previousElementSibling.value;
+  const searchUser = userArr.find(user => user.name == value);
 
+  let userName = searchUser.name;
+  let infoUser = refs.infoUser;
+  if (searchUser instanceof User) {
+    infoUser.children[0].textContent = 'User:';
+  }
 
-    const value=e.target.previousElementSibling.value;
-    const searchUser = userArr.find (user => user.name==value);
+  if (searchUser instanceof Admin) {
+    infoUser.children[0].textContent = 'Admin:';
+  }
 
-    let userType=searchUser.constructor.name;
-    let userName =searchUser.name;
-     console.log(searchUser.constructor.name);
-    let infoUser=refs.infoUser;
-    infoUser.children[0].textContent=userType+':';
-    infoUser.children[1].textContent=searchUser.name;
+  infoUser.children[1].textContent = userName;
 
-
-
-    
-
-    searchUser.filter();
-     if(searchUser instanceof Admin) {
-        refs.bntOpenPopupAddEvent.disabled=false;
-        refs.bntOpenPopupAddEvent.classList.add('is-active'); 
-        refs.table.classList.add('is-active');
-        searchUser.addEvent();
-        searchUser. removeEvent();
-     }
-    refs.popupLogin.classList.remove('is-open');
+  searchUser.filter();
+  if (searchUser instanceof Admin) {
+    refs.bntOpenPopupAddEvent.disabled = false;
+    refs.bntOpenPopupAddEvent.classList.add('is-active');
+    refs.table.classList.add('is-active');
+    searchUser.addEvent();
+    searchUser.removeEvent();
+  }
+  refs.popupLogin.classList.remove('is-open');
 }
-  
 
 export default login;
